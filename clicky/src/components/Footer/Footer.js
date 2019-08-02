ec2c7dbbca66b35b4fde9455cd5175ac1cf60f88
chris haynes <chrishaynes@chriss-mbp.home>
2019-08-02 19:18:52 -0400

    fixed className

--- clicky/src/components/Footer/Footer.js
+++ clicky/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div class="container">
+      <div className="container">
         <span className="footer">Clicky Game 2019</span>
       </div>
     </footer>
