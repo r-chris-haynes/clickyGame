c49f1fe8e56ebf4dd8bf780367477486151f5909
chris haynes <chrishaynes@chriss-MacBook-Pro.local>
2019-08-03 10:06:35 -0400

    Updates

---
+++ precache-manifest.cf553851f3262793c28316b763cd483e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e066da3e3c117b19080be4309a91198e",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "06dbc7a89599bf5b9e69",
+    "url": "/clicky/static/css/main.c949f6f3.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "06dbc7a89599bf5b9e69",
+    "url": "/clicky/static/js/main.f6f868ff.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  }
+]);
