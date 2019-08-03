8c0bdaaaf04be4968f900df5532f5ca656364018
chris haynes <chrishaynes@chriss-MacBook-Pro.local>
2019-08-03 10:14:54 -0400

    Updates

---
+++ precache-manifest.e72d06a155d190fb9afb528f584f278e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419e19a61c7a07b018343e0ff27197a0",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "06dbc7a89599bf5b9e69",
+    "url": "/clickyGame/static/css/main.c949f6f3.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "06dbc7a89599bf5b9e69",
+    "url": "/clickyGame/static/js/main.f6f868ff.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
