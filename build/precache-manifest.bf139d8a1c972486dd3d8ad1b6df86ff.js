c11352ca975f24c7c348b694fba92e48a4e418fe
chris haynes <r.chris.haynes@gmail.com>
2019-10-01 16:55:34 -0400

    updated readme

---
+++ build/precache-manifest.bf139d8a1c972486dd3d8ad1b6df86ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd19c720fd81a51e6587646d8ecc205",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "69d6cbf0ce4d1f8d8ad0",
+    "url": "/clickyGame/static/css/main.16d7626f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "69d6cbf0ce4d1f8d8ad0",
+    "url": "/clickyGame/static/js/main.959d3ad9.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
