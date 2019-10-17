cb2a80901962e5a85a52a06169b3b1a293ba4ebf
chris haynes <r.chris.haynes@gmail.com>
2019-10-17 14:50:13 -0400

    remove weird index.html

--- build/precache-manifest.e78e82bd78ebc26275a84723b90f9ea5.js
+++ build/precache-manifest.e78e82bd78ebc26275a84723b90f9ea5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3fd19c720fd81a51e6587646d8ecc205",
+    "revision": "fa9e06f611d68455cbd795a2c9c9a4f9",
     "url": "/clickyGame/index.html"
   },
   {
-    "revision": "69d6cbf0ce4d1f8d8ad0",
-    "url": "/clickyGame/static/css/main.16d7626f.chunk.css"
+    "revision": "efdbb31c7bf82e44db7a",
+    "url": "/clickyGame/static/css/main.2e0119a1.chunk.css"
   },
   {
     "revision": "45124f853ba07aa965c0",
     "url": "/clickyGame/static/js/2.6efc73d3.chunk.js"
   },
   {
-    "revision": "69d6cbf0ce4d1f8d8ad0",
-    "url": "/clickyGame/static/js/main.959d3ad9.chunk.js"
+    "revision": "efdbb31c7bf82e44db7a",
+    "url": "/clickyGame/static/js/main.406e1136.chunk.js"
   },
   {
     "revision": "95f5ce002aae55a30c31",
